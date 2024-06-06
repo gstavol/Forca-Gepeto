65f668b93237e99264589004644e7e2bebd056ce
Brunikito <90849953+Brunikito@users.noreply.github.com>
2024-06-05 23:57:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ while True:
     minha_key = input("Digite a sua key, por favor:\n")
     if minha_key != "":
         break
+#############################################
 
 client = OpenAI(api_key=minha_key)
 
-# Recebendo o prompt
+# Prompt de pergunta
 def prompt_gpt( Idioma="portugês", Tema="aleatório", Dificuldade="média"):
     prompt = f"""Preciso que gere uma lista, para meu jogo de forca, com uma palavra e uma dica seguindo esses requisitos:
     Idioma:{Idioma} (deve ser o idioma da resposta e da dica),
@@ -25,7 +26,9 @@ def prompt_gpt( Idioma="portugês", Tema="aleatório", Dificuldade="média"):
     respostacerta = json.loads(respostacerta)
 
     return respostacerta
+#############################################
 
+# Pergunta ao GPT
 def pergunta_gpt(pergunta):
 
     prompt = [{"role": "user", "content": pergunta }]
@@ -38,41 +41,46 @@ def pergunta_gpt(pergunta):
     )
 
     return resposta.choices[0].message.content
+#############################################
 
 def limpar_tela():
-    # print()
     os.system('cls' if os.name == 'nt' else 'clear')
 
+# Função principal
 def play_game():
+    # Configurações iniciais
     limpar_tela()
     IDIOMA = input("Digite um idioma: ")
     TEMA = input("Digite um tema: ")
     DIFICULDADE = input("Digite a dificuldade: ")
 
+    # Configurações da palvra recebida
     palavra_recebida = prompt_gpt(IDIOMA, TEMA, DIFICULDADE)
-    # print("resposta do gpto",type(palavra_recebida))
     palavra_correta = palavra_recebida[0]
     word_to_guess = funcoes.limpar_palavra(palavra_recebida[0])
-    # print("testar se e lista: ", word_to_guess)
     guessed_letters = []
     attempts = 6
 
+    # Jogo em si
     while attempts > 0:
         palavra_ate_agora = funcoes.display_word(word_to_guess, guessed_letters)
+        
+        # Verifica se o jogador ganhou
         if palavra_ate_agora == word_to_guess:
             limpar_tela()
             print("Parabéns! Você ganhou!, a palavra era", palavra_correta)
             break
     
-
         limpar_tela()
-        
+
+        # Dica da nossa querida Miku
         print("Dica da mamãe Miku: ", palavra_recebida[1])
         print()
 
-        
+        # Desenha a força
         print(funcoes.desenhar_forca(attempts))
 
+        # Parte das palavras
         print("Palavra:", funcoes.display_word(word_to_guess, guessed_letters))
         print("Letras:", end=" ")
         for i in sorted(guessed_letters):
@@ -91,11 +99,13 @@ def play_game():
         if guess not in word_to_guess:
             attempts -= 1
 
+    # Verifica se o jogador perdeu
     if attempts == 0:
         limpar_tela()
         print("Você perdeu! A palavra era:", palavra_correta)
+#############################################
 
-
+# Deseja continuar?
 while True:
     play_game()
     limpar_tela
